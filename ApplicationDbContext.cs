1250e4f5cf45ec2c793f3225e23965ac2551cc54
RCankov <re.cankov@gmail.com>
2025-01-02 21:11:10 +0100

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Athletics.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Security.Cryptography.Xml;
+using System.Text.RegularExpressions;
+
+namespace Athletics
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Athlete> Athletes { get; set; }
+        public DbSet<Discipline> Disciplines { get; set; }
+        public DbSet<Result> Results { get; set; }
+
+    }
+}
